43703f09cb7073bd45338403707306f6de90950d
DESKTOP-N8VJUR3\wqs <1201013@qq.com>
2021-12-24 13:12:55 +0800

    add sleep func

---
+++ util/sleep.go
@@ -0,0 +1,28 @@
+package util
+
+import "syscall"
+
+func Sleep(ms int64) {
+	var tv syscall.Timeval
+	_ = syscall.Gettimeofday(&tv)
+
+	startTick := int64(tv.Sec)*int64(1000000) + int64(tv.Usec) + ms*1000
+	endTick := int64(0)
+	for endTick < startTick {
+		_ = syscall.Gettimeofday(&tv)
+		endTick = int64(tv.Sec)*int64(1000000) + int64(tv.Usec)
+	}
+}
+
+
+func USleep(us int64) {
+	var tv syscall.Timeval
+	_ = syscall.Gettimeofday(&tv)
+
+	startTick := int64(tv.Sec)*int64(1000000) + int64(tv.Usec) + us
+	endTick := int64(0)
+	for endTick < startTick {
+		_ = syscall.Gettimeofday(&tv)
+		endTick = int64(tv.Sec)*int64(1000000) + int64(tv.Usec)
+	}
+}
