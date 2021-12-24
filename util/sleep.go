029c4c1640e820587fe443c7278aa718397d4184
DESKTOP-N8VJUR3\wqs <1201013@qq.com>
2021-12-24 13:50:18 +0800

    add sleep

--- util/sleep.go
+++ util/sleep.go
@@ -14,6 +14,21 @@ func Sleep(ms int64) {
 	}
 }
 
+func SleepUtil(ms int64,f func()bool) {
+	var tv syscall.Timeval
+	_ = syscall.Gettimeofday(&tv)
+
+	startTick := int64(tv.Sec)*int64(1000000) + int64(tv.Usec) + ms*1000
+	endTick := int64(0)
+	for endTick < startTick {
+		if f() {
+			break
+		}
+		_ = syscall.Gettimeofday(&tv)
+		endTick = int64(tv.Sec)*int64(1000000) + int64(tv.Usec)
+	}
+}
+
 
 func USleep(us int64) {
 	var tv syscall.Timeval
