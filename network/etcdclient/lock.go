3366531928424ec30c3163ce3f65375f7b93b208
wqs <1201013@qq.com>
2022-02-18 09:56:38 +0800

    fix etcd lock

--- network/etcdclient/lock.go
+++ network/etcdclient/lock.go
@@ -15,26 +15,26 @@ import (
 )
 
 const (
-	defaultTTL   = 15
-	defaultTry   = 3
-	prefix = "/mutex"
+	defaultTTL = 15
+	defaultTry = 3
+	prefix     = "/mutex"
 )
 
 type Mutex struct {
-	key    string
-	id     string // The identity of the caller
-	client *clientv3.Client
-	kapi   clientv3.KV
-	ctx    context.Context
-	ttl    int64
-	mutex  *sync.Mutex
-	logger io.Writer
-	lease clientv3.Lease
+	key     string
+	id      string // The identity of the caller
+	client  *clientv3.Client
+	kapi    clientv3.KV
+	ctx     context.Context
+	ttl     int64
+	mutex   *sync.Mutex
+	logger  io.Writer
+	lease   clientv3.Lease
 	watcher clientv3.Watcher
 	leaseId clientv3.LeaseID
 }
 
-func CreateMutex(c *clientv3.Client,key string, ttl int) (*Mutex, error) {
+func CreateMutex(c *clientv3.Client, key string, ttl int) (*Mutex, error) {
 
 	hostname, err := os.Hostname()
 	if err != nil {
@@ -61,7 +61,6 @@ func CreateMutex(c *clientv3.Client,key string, ttl int) (*Mutex, error) {
 		kapi:   clientv3.NewKV(c),
 		ctx:    context.TODO(),
 		ttl:    int64(ttl),
-
 	}, nil
 }
 
@@ -86,45 +85,48 @@ func (this *Mutex) Lock() (err error) {
 func (this *Mutex) lock() (err error) {
 	log.Info("trying to create a node", LogKey(prefix+this.key))
 	for {
-		leaseResp,err:=this.lease.Grant(this.ctx,this.ttl)
+		var leaseResp *clientv3.LeaseGrantResponse
+		leaseResp, err = this.lease.Grant(this.ctx, this.ttl)
 		if err != nil {
 			log.Error(err.Error())
 			return err
 		}
 		this.leaseId = leaseResp.ID
-		resp, err := this.kapi.Put(this.ctx, prefix+this.key, this.id, clientv3.WithPrevKV(),clientv3.WithLease(leaseResp.ID))
-		if err == nil&&resp.PrevKv==nil {
-			log.Info("create node",LogKey(this.key), LogOk(true),LogResp(resp))
+		var resp *clientv3.PutResponse
+		resp, err = this.kapi.Put(this.ctx, prefix+this.key, this.id, clientv3.WithPrevKV(), clientv3.WithLease(leaseResp.ID))
+		if err == nil && resp.PrevKv == nil {
+			log.Info("create node", LogKey(this.key), LogOk(true), LogResp(resp))
 			return nil
 		}
-		log.Error("create node",LogKey(this.key), LogOk(false),zap.Error(err))
 		if err != nil {
+			log.Error("create node", LogKey(this.key), LogOk(false), zap.Error(err))
 			log.Error(err.Error())
 			return err
 		}
-
-		gResp, err := this.kapi.Get(this.ctx, this.key)
+		var gResp *clientv3.GetResponse
+		gResp, err = this.kapi.Get(this.ctx, this.key)
 		if err != nil {
+			log.Error("get node", LogKey(this.key), LogOk(false), zap.Error(err))
 			return err
 		}
-		log.Error("get node",LogKey(this.key), LogOk(true))
-		watcher := this.watcher.Watch(this.ctx,prefix+this.key, clientv3.WithRev(gResp.Header.Revision))
-		log.Error("watching start",LogKey(this.key))
+		log.Error("get node", LogKey(this.key), LogOk(true))
+		watcher := this.watcher.Watch(this.ctx, prefix+this.key, clientv3.WithRev(gResp.Header.Revision))
+		log.Error("watching start", LogKey(this.key))
 		for {
 			select {
-			case wResp:=<-watcher:
-				if wResp.Err()!=nil {
+			case wResp := <-watcher:
+				if wResp.Err() != nil {
 					return err
 				}
-				for _,e:=range wResp.Events {
-					if e.Type  == clientv3.EventTypeDelete {
+				for _, e := range wResp.Events {
+					if e.Type == clientv3.EventTypeDelete {
 						goto LoopEnd
 					}
-					log.Info("received an event", zap.String("event",log.PrettyStruct(e)))
+					log.Info("received an event", zap.String("event", log.PrettyStruct(e)))
 				}
 			}
 		}
-		LoopEnd:
+	LoopEnd:
 	}
 	return err
 }
@@ -137,10 +139,10 @@ func (this *Mutex) Unlock() (err error) {
 		var resp *clientv3.DeleteResponse
 		resp, err = this.kapi.Delete(this.ctx, prefix+this.key)
 		if err == nil {
-			log.Info("etcd lock",LogKey(this.key),LogAction(ETCD_DELETE), LogOk(true))
+			log.Info("etcd lock", LogKey(this.key), LogAction(ETCD_DELETE), LogOk(true))
 			return nil
 		}
-		log.Info("etcd lock",LogKey(this.key),LogAction(ETCD_DELETE), LogOk(false),LogResp(resp))
+		log.Info("etcd lock", LogKey(this.key), LogAction(ETCD_DELETE), LogOk(false), LogResp(resp))
 
 		if err == rpctypes.ErrEmptyKey {
 			return nil
@@ -151,11 +153,11 @@ func (this *Mutex) Unlock() (err error) {
 }
 
 func (this *Mutex) RefreshLockTTL(ttl time.Duration) (err error) {
-	resp,err:=this.lease.KeepAliveOnce(context.TODO(),this.leaseId)
+	resp, err := this.lease.KeepAliveOnce(context.TODO(), this.leaseId)
 	if err != nil {
-		log.Error("refresh ttl",LogKey(this.key), LogOk(false), LogOk(false),LogResp(resp))
+		log.Error("refresh ttl", LogKey(this.key), LogOk(false), LogOk(false), LogResp(resp))
 	} else {
-		log.Error("refresh ttl",LogKey(this.key), LogOk(true))
+		log.Error("refresh ttl", LogKey(this.key), LogOk(true))
 	}
 	return err
 }
