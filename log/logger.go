2083e5e2b8f618f70ac7b5412418f472da309231
wqs <1201013@qq.com>
2022-09-13 11:02:44 +0800

    fix:add default log config

--- log/logger.go
+++ log/logger.go
@@ -11,90 +11,119 @@ import (
 	"time"
 )
 
-var __hooks []Hook = make([]Hook,0)
+var __hooks []Hook = make([]Hook, 0)
 var once sync.Once
 var _logger *ComposeLogger
 var _console bool
 
-
 type titleEncoder struct {
-	title string
+	title                       string
 	levelToLowercaseColorString map[zapcore.Level]string
-	levelToCapitalColorString map[zapcore.Level]string
+	levelToCapitalColorString   map[zapcore.Level]string
 }
 
-func newTitleEncoder(title string)*titleEncoder {
-	ret:= &titleEncoder{
+func newTitleEncoder(title string) *titleEncoder {
+	ret := &titleEncoder{
 		title:                       "",
 		levelToLowercaseColorString: make(map[zapcore.Level]string, len(_levelToColor)),
 		levelToCapitalColorString:   make(map[zapcore.Level]string, len(_levelToColor)),
 	}
 	for level, color := range _levelToColor {
-		ret.levelToLowercaseColorString[level] = color.Add("["+level.String()+"]"+title)
-		ret.levelToCapitalColorString[level] = color.Add("["+level.CapitalString()+"]"+title)
+		ret.levelToLowercaseColorString[level] = color.Add("[" + level.String() + "]" + title)
+		ret.levelToCapitalColorString[level] = color.Add("[" + level.CapitalString() + "]" + title)
 	}
 	return ret
 }
 
-func (this *titleEncoder)Encode(l zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+func (this *titleEncoder) Encode(l zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
 	s, ok := this.levelToCapitalColorString[l]
 	if !ok {
-		s = _unknownLevelColor.Add(l.CapitalString()+this.title)
+		s = _unknownLevelColor.Add(l.CapitalString() + this.title)
 	}
 	enc.AppendString(s)
 }
 
-func newEncoderConfig(title string,console bool)*zapcore.EncoderConfig{
-	if console {
-		return &zapcore.EncoderConfig{
-			TimeKey:        "time",
-			LevelKey:       "level",
-			NameKey:        "logger",
-			CallerKey:      "caller",
-			MessageKey:     "msg",
-			StacktraceKey:  "stacktrace",
-			LineEnding:     zapcore.DefaultLineEnding,
-			EncodeLevel:    newTitleEncoder(title).Encode, //这里可以指定颜色
-			EncodeTime:     TimeEncoder,                      // ISO8601 UTC 时间格式
-			EncodeDuration: zapcore.SecondsDurationEncoder,
-			EncodeCaller:   zapcore.ShortCallerEncoder, // 全路径编码器
-		}
-	} else {
-		return &zapcore.EncoderConfig{
-			LevelKey:       "level",
-			NameKey:        "logger",
-			CallerKey:      "caller",
-			MessageKey:     "msg",
-			StacktraceKey:  "stacktrace",
-			LineEnding:     zapcore.DefaultLineEnding,
-			EncodeLevel:    zapcore.CapitalLevelEncoder, //这里可以指定颜色
-			EncodeTime:     nil,                                                             // ISO8601 UTC 时间格式
-			EncodeDuration: zapcore.SecondsDurationEncoder,
-			EncodeCaller:   zapcore.ShortCallerEncoder, // 全路径编码器
-		}
+func DefaultConsoleConfig(title string) *zapcore.EncoderConfig {
+	return &zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    newTitleEncoder(title).Encode, //这里可以指定颜色
+		EncodeTime:     TimeEncoder,                   // ISO8601 UTC 时间格式
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder, // 全路径编码器
 	}
 }
 
+var DefaultJsonConfig = zapcore.EncoderConfig{
+	LevelKey:       "level",
+	NameKey:        "logger",
+	CallerKey:      "caller",
+	MessageKey:     "msg",
+	StacktraceKey:  "stacktrace",
+	LineEnding:     zapcore.DefaultLineEnding,
+	EncodeLevel:    zapcore.CapitalLevelEncoder, //这里可以指定颜色
+	EncodeTime:     nil,                         // ISO8601 UTC 时间格式
+	EncodeDuration: zapcore.SecondsDurationEncoder,
+	EncodeCaller:   zapcore.ShortCallerEncoder, // 全路径编码器
+}
+
+var DefaultJsonConfigWithTime = zapcore.EncoderConfig{
+	TimeKey:        "time",
+	LevelKey:       "level",
+	NameKey:        "logger",
+	CallerKey:      "caller",
+	MessageKey:     "msg",
+	StacktraceKey:  "stacktrace",
+	LineEnding:     zapcore.DefaultLineEnding,
+	EncodeLevel:    zapcore.CapitalLevelEncoder, //这里可以指定颜色
+	EncodeTime:     TimeEncoder,                 // ISO8601 UTC 时间格式
+	EncodeDuration: zapcore.SecondsDurationEncoder,
+	EncodeCaller:   zapcore.ShortCallerEncoder, // 全路径编码器
+}
+
+func NewEncoderConfig(title string, console bool) *zapcore.EncoderConfig {
+	if console {
+		return DefaultConsoleConfig(title)
+	} else {
+		return &DefaultJsonConfig
+	}
+}
+
+func CustomConfig(json, console, jsonConsole, object bool, config *zapcore.EncoderConfig) *LogConfig {
+	return &LogConfig{
+		Title:       "",
+		EncodeCfg:   config,
+		Console:     console,
+		JsonConsole: jsonConsole,
+		Json:        json,
+		Object:      object,
+	}
+}
 
 func ConsoleConfig(title string) *LogConfig {
 	return &LogConfig{
-		Title: "",
-		EncodeCfg: newEncoderConfig(title,true) ,
-		Console:   true,
+		Title:       "",
+		EncodeCfg:   NewEncoderConfig(title, true),
+		Console:     true,
 		JsonConsole: false,
-		Json:      false,
-		Object:    false,
+		Json:        false,
+		Object:      false,
 	}
 }
 
 func DefaultConfig(title string) *LogConfig {
 	return &LogConfig{
-		Title: "",
-		EncodeCfg: newEncoderConfig(title,false) ,
-		Console:   true,
+		Title:       "",
+		EncodeCfg:   NewEncoderConfig(title, false),
+		Console:     true,
 		JsonConsole: true,
-		Json:      true,
-		Object:    false,
+		Json:        true,
+		Object:      false,
 	}
 }
 
@@ -109,10 +138,10 @@ func newLogConfig() zap.Config {
 		DisableCaller:     false,
 		DisableStacktrace: true,
 		Sampling:          nil,
-		Encoding:          "console",                // 输出格式 console 或 json
-		EncoderConfig:     *newEncoderConfig("",true),            // 编码器配置
-		InitialFields:     map[string]interface{}{}, // 初始化字段，如：添加一个服务器名称
-		OutputPaths:       []string{"stdout"},       // 输出到指定文件 stdout（标准输出，正常颜色） stderr（错误输出，红色）
+		Encoding:          "console",                   // 输出格式 console 或 json
+		EncoderConfig:     *NewEncoderConfig("", true), // 编码器配置
+		InitialFields:     map[string]interface{}{},    // 初始化字段，如：添加一个服务器名称
+		OutputPaths:       []string{"stdout"},          // 输出到指定文件 stdout（标准输出，正常颜色） stderr（错误输出，红色）
 		ErrorOutputPaths:  []string{"stderr"},
 	}
 }
@@ -135,8 +164,8 @@ type Field struct {
 
 type Fields map[string]interface{}
 
-func InitDefault(dev bool,console bool,title string, hooks ...Hook){
-	for _,h:=range __hooks {
+func InitDefault(dev bool, console bool, title string, hooks ...Hook) {
+	for _, h := range __hooks {
 		hooks = append(hooks, h)
 	}
 	if console {
@@ -148,12 +177,12 @@ func InitDefault(dev bool,console bool,title string, hooks ...Hook){
 	}
 }
 
-func IsConsole()bool {
+func IsConsole() bool {
 	return _console
 }
 
 func Init(dev bool, conf *LogConfig, hooks ...Hook) {
-	for _,h:=range __hooks {
+	for _, h := range __hooks {
 		hooks = append(hooks, h)
 	}
 	_logger = NewComposeLogger(dev, conf, 2, hooks...)
@@ -163,8 +192,8 @@ func AddHook(hook Hook) {
 	if _logger == nil {
 		Init(true, DefaultConfig(""))
 	}
-	for _,v:=range __hooks {
-		if v==hook {
+	for _, v := range __hooks {
+		if v == hook {
 			return
 		}
 	}
@@ -176,14 +205,14 @@ func RemoveHook(hook Hook) {
 	if _logger == nil {
 		Init(true, DefaultConfig(""))
 	}
-	ret:=make([]Hook,0)
-	for index,v:=range __hooks {
+	ret := make([]Hook, 0)
+	for index, v := range __hooks {
 		ret = __hooks[:index]
-		if v==hook {
+		if v == hook {
 			if index == len(__hooks)-2 {
 				break
 			}
-			for _,w:=range __hooks[index+1:] {
+			for _, w := range __hooks[index+1:] {
 				ret = append(ret, w)
 			}
 			break
@@ -197,12 +226,11 @@ func Close() {
 	}
 }
 
-func SetDefaultLogger(logger *ComposeLogger){
+func SetDefaultLogger(logger *ComposeLogger) {
 	_logger = logger
 }
 
-
-func DefaultLogger()*ComposeLogger{
+func DefaultLogger() *ComposeLogger {
 	if _logger == nil {
 		Init(true, DefaultConfig(""))
 	}
@@ -213,22 +241,22 @@ func WithFields(fields Fields) *ComposeLogger {
 	return _logger.WithFields(fields)
 }
 
-func WithField(key string,value interface{})*ComposeLogger {
-	return _logger.WithField(key,value)
+func WithField(key string, value interface{}) *ComposeLogger {
+	return _logger.WithField(key, value)
 }
 
 var reg1 = regexp.MustCompile(`(%v)|(%[+]v)|(%[#]v )|(%T)|(%t)|(%b )|(%c)|(%d)|(%o)|(%q)|(%x)|(%X)|(%U)|(%b)|(%e)|(%E)|(%[[.]|(0-9)]*f)|(%[[.]|(0-9)]*g)|(%[[.]|(0-9)]*G)|(%s)|(%q)|(%f)|(%x)|(%X)|(%p)`)
 
 func sprintf(args []interface{}) interface{} {
 	ret := make([]interface{}, 0)
-	if len(args)==1 {
+	if len(args) == 1 {
 		return args[0]
 	}
-	if len(args)>1 {
-		if reflect.TypeOf(args[0]).Kind() == reflect.String{
-			argstr:=args[0].(string)
+	if len(args) > 1 {
+		if reflect.TypeOf(args[0]).Kind() == reflect.String {
+			argstr := args[0].(string)
 			if reg1.MatchString(argstr) {
-				return fmt.Sprintf(argstr,args[1:]...)
+				return fmt.Sprintf(argstr, args[1:]...)
 			}
 		}
 	}
@@ -270,7 +298,7 @@ type ILogger interface {
 	Panic(msg string, fields ...zap.Field)
 	Fatal(msg string, fields ...zap.Field)
 	WriteString(s string)
-	Write(p []byte)(int,error)
+	Write(p []byte) (int, error)
 }
 
 type StdLogger interface {
@@ -380,7 +408,7 @@ func Debug(msg string, fields ...zap.Field) {
 	_logger.Debug(msg, fields...)
 }
 
-func Error(msg string, fields ...zap.Field)error {
+func Error(msg string, fields ...zap.Field) error {
 	if _logger == nil {
 		Init(true, DefaultConfig(""))
 	}
@@ -401,14 +429,13 @@ func Fatal(msg string, fields ...zap.Field) {
 	_logger.Fatal(msg, fields...)
 }
 
-
-func Sync()error{
+func Sync() error {
 	if _logger == nil {
 		return nil
 	}
 	return _logger.Sync()
 }
 
-func PrettyStruct(s interface{})string {
-	return fmt.Sprintf("%+v",s)
+func PrettyStruct(s interface{}) string {
+	return fmt.Sprintf("%+v", s)
 }
