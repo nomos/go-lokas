496db5addb111993c52cc3c8fd06184a9e39691f
龚国玮 <gongguowei01@gmail.com>
2022-11-30 17:56:28 +0800

    feat(dockerclient): get services

---
+++ network/dockerclient/swarm_service.go
@@ -0,0 +1,21 @@
+package dockerclient
+
+import (
+	"encoding/json"
+	"github.com/docker/docker/api/types/swarm"
+	"net/http"
+)
+
+func (c *TLSClient) ListServices() ([]swarm.Service, error) {
+	var services []swarm.Service
+	path := "/services?"
+	resp, err := c.do(http.MethodGet, path, doOptions{})
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if err := json.NewDecoder(resp.Body).Decode(&services); err != nil {
+		return nil, err
+	}
+	return services, nil
+}
